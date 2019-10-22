ad92063c92c0e436f420cddf080b5aa1032a7153
Siddhant Gupta <me@guptasiddhant.com>
2019-10-22 17:07:26 +0300

    Updates

---
+++ precache-manifest.20bc1040fc509ea57780e80e721072ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d75176cc3c53fdebed7566698da2f7",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "e0d478e880ccb08cad15",
+    "url": "/react/static/css/main.2c886a61.chunk.css"
+  },
+  {
+    "revision": "99aec923a529e6a4234f",
+    "url": "/react/static/js/2.b86f22bc.chunk.js"
+  },
+  {
+    "revision": "e0d478e880ccb08cad15",
+    "url": "/react/static/js/main.0c1c4cfc.chunk.js"
+  },
+  {
+    "revision": "688d60c7f530855f2ff3",
+    "url": "/react/static/js/runtime-main.4830ebc2.js"
+  }
+]);
